61257140809937b2f5b851ba8521edf0b96ed1ef
ceefit <ceefit@protonmail.com>
2020-11-24 11:00:30 -0500

    It uploads and downloads files from a Vultr object storage bucket

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-var express = require('express');
-var session = require('express-session');
-var bodyParser = require('body-parser');
-var Keycloak = require('keycloak-connect');
-var cors = require('cors');
+const AWS = require('aws-sdk');
+const Keycloak = require('keycloak-connect');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
+const multer  = require('multer');
+const multerS3 = require('multer-s3')
+const session = require('express-session');
 
-var app = express();
+AWS.config.credentials = new AWS.SharedIniFileCredentials({profile: 'dc-demo'});;
+const spacesEndpoint = new AWS.Endpoint('ewr1.vultrobjects.com');
+s3 = new AWS.S3({apiVersion: '2006-03-01', endpoint: spacesEndpoint});
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'dc-demo',
+    metadata: function (req, file, cb) {
+      cb(null, {fieldName: file.fieldname});
+    },
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString())
+    }
+  })
+})
+
+const app = express();
 app.use(bodyParser.json());
 
 // Enable CORS support
@@ -13,7 +33,7 @@ app.use(cors());
 // Create a session-store to be used by both the express-session
 // middleware and the keycloak middleware.
 
-var memoryStore = new session.MemoryStore();
+const memoryStore = new session.MemoryStore();
 
 app.use(session({
   secret: 'super secret',
@@ -22,7 +42,7 @@ app.use(session({
   store: memoryStore
 }));
 
-var keycloak = new Keycloak({
+const keycloak = new Keycloak({
   store: memoryStore
 });
 
@@ -30,15 +50,51 @@ app.use(keycloak.middleware({
         logout: '/logout'
     }));
 
-
 app.get('/', function (req, res) {
   res.send("HOME");
 });
 
-
 app.get('/logout', function (req, res) {
   res.json({message: 'logged out'});
 });
+
+app.get('/bucket-list', function (req, res) {
+  var params = {
+    Bucket: "dc-demo",
+    MaxKeys: 25 // The max number of objects to list
+  };
+  s3.listObjectsV2(params, function(err, data) {
+    if (err) {
+      res.json({message: 'error', error: err})
+    } else {
+      res.json({objects: data.Contents});
+    }
+  });
+})
+
+app.post('/bucket-upload', upload.single('selectedAudioFileField'), (req, res, next) => {
+  res.send('Successfully uploaded'); // TODO: Some error checking here would be nice
+});
+
+app.get('/bucket-download/:key', function (req, res) {
+  var key = req.params.key;
+
+  s3.getObject(
+      { Bucket: "dc-demo", Key: key },
+      function (error, data) {
+        if (error != null) {
+          res.send("Failed to retrieve an object: " + error);
+        } else {
+          res.writeHead(200, {
+            'Content-Type': 'application/mp3',
+            'Content-Disposition': `'attachment; filename=${key}'`,
+            'Content-Length': data.Body.length
+          });
+          res.end(data.Body);
+        }
+      }
+  );
+})
 
 
 app.get('/service/public', function (req, res) {
